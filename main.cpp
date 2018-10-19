1b6eaeda9c92e6bbc33c2678767f9aa2a091cc51
payaln <payaln@mail.ru>
2018-10-19 14:58:16 +0300

    Ex1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 float too (float a) {
-    if (a==1)
-    std::cout << "Hello, World!1" << std::endl;
-    std::cout << "Hello, World!2" << std::endl;
-    std::cout << "Hello, World!3" << std::endl;
+    if (a==1) {
+        std::cout << "Hello, World!1" << std::endl;
+        std::cout << "Hello, World!2" << std::endl;
+        std::cout << "Hello, World!3" << std::endl;
+    }
 }
 
 float too1 (float a) {
