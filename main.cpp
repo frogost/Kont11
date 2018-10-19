6d35345b82c46e4a71d68b235bc4880e093251c8
payaln <payaln@mail.ru>
2018-10-19 14:56:02 +0300

    Ex1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+float too (float a) {
+    if (a==1)
+    std::cout << "Hello, World!1" << std::endl;
+    std::cout << "Hello, World!2" << std::endl;
+    std::cout << "Hello, World!3" << std::endl;
+}
+
+float too1 (float a) {
+    a++;
+    return too(a);
+}
+
+int main() {
+    float a;
+    std::cout << too1 (a) << std::endl;
+    return 0;
+}
