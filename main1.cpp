e6e519089a9b2207812f15b34ebc03569aa72b7d
payaln <payaln@mail.ru>
2018-10-19 16:35:03 +0300

    Ex2

--- main1.cpp
+++ main1.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <math.h>
 
-float too (float m, float t, float x) {
-    float res;
-    res=(1/(t*sqrt(2*3.14)))*exp(-(pow((x-m),2))/(2*pow(t,2)));
-    return res;
+double too (float m, float t, float x) {
+    return (1/(t*sqrt(2*3.14)))*exp(-(pow((x-m),2))/(2*pow(t,2)));
 }
 
 int main() {
-    float m;
-    float t;
-    float x;
+    float m=5;
+    float t=5;
+    float x=5;
     std::cout << too (m,t,x) << std::endl;
     return 0;
 }
